3f3d5feafe0e36f648dfaa28b480eaf8a040f675
Jeremy Chan <jeremyczh@gmail.com>
2022-02-10 16:00:41 +0800

    final

---
+++ src/robots.js
@@ -0,0 +1,33 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Jeremy Chan',
+        username: 'jeremyczh',
+        email: 'jeremyczh@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Marcus Rashford',
+        username: 'rashfordm',
+        email: 'rashfordm@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Mason Mount',
+        username: 'mmount',
+        email: 'mmount@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Mo Salah',
+        username: 'salahmo',
+        email: 'salahmo@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Jo Salah',
+        username: 'adsf',
+        email: 'salahmo@gmail.com'
+    }
+
+]
